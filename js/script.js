b220d540237441e783ae2239db46f5514616621d
Edward Yeh <scribbleboyed@gmail.com>
2015-12-11 14:07:40 -0800

    added nav buttons

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
+// URL STRINGS
+
 var queryBaseURL = "https://demo1.kaleosoftware.com/v4/search.json?sitemap_token=123456789&sitemap=sales";
 var queryURL = queryBaseURL;
 
+// 
+
+var META;
 var RESULTS = [];
 var TAGS = [];
 
 var bodyContainer = $('#body');
+var navContainer = $('#nav-container');
 var resultsContainer = $('#results-container');
 var tagsContainer = $('#tags-container');
 var searchField = $('#search-field');
@@ -12,65 +18,88 @@ var searchButton = $('#search-button');
 
 var timeoutID = null;
 
+var navTemplate = _.template($('.nav-template').html())
 var tagTemplate = _.template($('.tag-template').html());
 var resultTemplate = _.template($('.result-template').html());
+
+// LOAD BASE URL QUERY ON PAGE LOAD
 
 $(document).ready(function() {
 	bodyContainer.hide();
    $.get(queryBaseURL)
    .done(function(results) {
+   	 META = results.meta;
+   	 console.log(META.next_link);
      RESULTS = results.collection;
      initPage();
    });
 });
+
+// DISPLAY INITIAL RESULTS
 
 var initPage = function() {
 
 	displayResults();
 };
 
+// DISPLAY RESULTS FUNCTION
+
 function displayResults() {
 
+	navContainer.hide();
 	bodyContainer.hide();
+	tagsContainer.hide();
 
+	navContainer.empty();
 	resultsContainer.empty();
+	tagsContainer.empty();
 
-	if (RESULTS.length > 0) {
-		RESULTS.forEach(function(result) {
+	RESULTS.forEach(function(result) {
 
-			result.tag_names.forEach(function(tag) {
-				if (TAGS.indexOf(tag) < 0) {
-					TAGS.push(tag)
-				}
-			});
-
-			var compiledResult = resultTemplate({
-				title: result.title,
-				id: result.id,
-				status: result.status,
-				board_name: result.board_name,
-				tag_names: result.tag_names,
-				views_count: result.views_count,
-				answers_count: result.answers_count,
-				created_at: result.created_at,
-				url: result.url_anonymous
-			});
-			
-			resultsContainer.append(compiledResult);
+		result.tag_names.forEach(function(tag) {
+			if (TAGS.indexOf(tag) < 0) {
+				TAGS.push(tag)
+			}
 		});
-	} else {
-		resultsContainer.append('No Results Found')
-	}
+
+		var compiledResult = resultTemplate({
+			title: result.title,
+			id: result.id,
+			status: result.status,
+			board_name: result.board_name,
+			tag_names: result.tag_names,
+			views_count: result.views_count,
+			answers_count: result.answers_count,
+			created_at: result.created_at,
+			url: result.url_anonymous
+		});
+		
+		resultsContainer.append(compiledResult);
+	});
 
  	var compiledTags = tagTemplate({
 		tags: TAGS
 	});
 
+	var compiledNav = navTemplate({
+		total_results: META.total_results,
+		total_pages: META.total_pages,
+		page: META.page,
+		prev_link: "https://demo1.kaleosoftware.com" + META.prev_link,
+		next_link: "https://demo1.kaleosoftware.com" + META.next_link
+	});
+
 	tagsContainer.append(compiledTags);
 
+	navContainer.append(compiledNav);
+
 	bodyContainer.fadeIn(200);
+	tagsContainer.fadeIn(200);
+	navContainer.fadeIn(200);
 
 }
+
+// EXECUTE SEARCH WHEN USER STOPS TYPING
 
 searchField.keyup(function(e) {
 
@@ -85,20 +114,32 @@ searchField.keyup(function(e) {
 
 });
 
+// UPDATE RESULT FUNCTION
+
 function updateResults(url) {
 
 	RESULTS = [];
+	TAGS = [];
 
 	$.get(url)
 	.done(function(results) {
+		META = results.meta;
 		RESULTS = results.collection;
 		displayResults();
 	});
 
 }
 
+// TAG FILTER PRESSED
+
 function filterByTag(tag) {
 	var queryURLWithTag = queryURL + "&tags[]=" + tag.replace(/ /g, "&20");
-	console.log(queryURLWithTag);
 	updateResults(queryURLWithTag);
+}
+
+// NAV BUTTON PRESSED
+
+function searchByURL(navURL) {
+	console.log(navURL);
+	updateResults(navURL);
 }
